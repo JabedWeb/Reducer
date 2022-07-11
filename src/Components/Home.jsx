253eb37aebf88177dc1270c49dcce44b1beaf206
jabed366 <jabedhasan231@gmail.com>
2022-07-11 17:17:24 +0600

    alert

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useReducer } from 'react'
 
-import {Container,Col,Row,Card} from 'react-bootstrap'
+import {Container,Col,Row,Card, Alert} from 'react-bootstrap'
 import { initialMessage, messageReducer } from '../reducers/messageReducer';
 
 
@@ -10,17 +10,23 @@ const Home = () => {
   return (
     <Container className="my-4">
     <Row className="justify-content-center">
-    <Col md={5}>
+    <Col md={5}>+
       <Card className="shadow">
         <Card.Body>
+           {messageState.error && <Alert variant='danger'>Error Showing</Alert> }
         {/* <input type="text" value={msg} onChange={(e)=> dispatch({type : '',payload : ''})} className="form-control"/> */}
-        <input type="text" value={messageState.name} placeholder='name' onChange={(e)=> dispatch({ type : '', payload : {name : e.target.value}})} className="form-control"/>
+        <input type="text" value={messageState.name} placeholder='name' onChange={(e)=> dispatch({ type : 'DEVS_DATA_LOAD', payload : {name : e.target.value}})} className="form-control"/>
         <br/>
-        <input type="text" placeholder='Email' value={messageState.age} onChange={(e)=> dispatch({ type : '', payload :{ age : e.target.value}})}className="form-control"/>
+        <input type="text" placeholder='Email' value={messageState.age} onChange={(e)=> dispatch({ type : 'DEVS_DATA_LOAD', payload :{ age : e.target.value}})}className="form-control"/>
         <br/>
-        <input type="text" placeholder='Skill' value={messageState.skill} onChange={(e)=> dispatch({ type : '', payload :{ skill : e.target.value}})} className="form-control"/>
+        <input type="text" placeholder='Skill' value={messageState.skill} onChange={(e)=> dispatch({ type : 'DEVS_DATA_LOAD', payload :{ skill : e.target.value}})} className="form-control"/>
         <br/>
         {/* <input type="text" value={msg} onChange={(e)=> dispatch({name : "Jabed",age : 20})} className="form-control"/> */}
+        <select onChange={(e)=> dispatch({ type : 'AlERT_LOAD', payload :{ error : e.target.value}})} className="form-control" name="" id="">
+          <option value="Start">Start</option>
+          <option value="Off">Off</option>
+        </select>
+        
         <p>Name : {messageState.name}</p>
         <p>Email : {messageState.age}</p>
         <p>Skill : {messageState.skill}</p>
--- src/reducers/messageReducer.js
+++ src/reducers/messageReducer.js
@@ -4,11 +4,23 @@ export const initialMessage={
     name : 'alo',
     age : 120,
     skill : 'Ios Developer',
+    error : false
 };
 
 
 //message reducer
 export const messageReducer= (state,{type,payload})=>{
-    return state ={...state,...payload};
+    
+    switch(type){
+        case 'DEVS_DATA_LOAD' :
+        return state ={...state,...payload};
 
+        case 'AlERT_LOAD' : 
+        let error_val=(payload.error=="Start") ? true : false;
+        return state ={...state,error : error_val};
+
+        default : 
+         return state;
+
+    }
 }
