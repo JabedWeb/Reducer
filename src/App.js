2a61e0fc2cb7e4f9761c4f3e038ae5c11bdd9d56
jabed366 <jabedhasan231@gmail.com>
2022-07-11 13:36:55 +0600

    Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import Home from "./Components/Home";
-import {Container,Col,Row,Card} from 'react-bootstrap'
-import { useState } from "react";
 function App() {
 
-  const [msg,setMsg]=useState('I love Reducer');
+
   return (
    <>
+  <Home/>
 
-  <Container className="my-4">
-    <Row className="justify-content-center">
-    <Col md={5}>
-      <Card className="shadow">
-        <Card.Body>
-        <input type="text" className="form-control"/>
-        <h1>{msg}</h1>
-        </Card.Body>
-      </Card>
-    </Col>
-    </Row>
-  </Container>
 
    </>
   );
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+
+import {Container,Col,Row,Card} from 'react-bootstrap'
+import { useState } from "react";
+
+// action catch data;
+const reducer= (state,action)=>{
+    return state =action.payload;
+}
 
 const Home = () => {
+
+    //const [msg,setMsg]=useState('I love Reducer');
+
+    const [msg,dispatch]=useReducer(reducer,"I love you");
+
+
+
+
   return (
-    <div>Home</div>
+    <Container className="my-4">
+    <Row className="justify-content-center">
+    <Col md={5}>
+      <Card className="shadow">
+        <Card.Body>
+        {/* <input type="text" value={msg} onChange={(e)=> dispatch({type : '',payload : ''})} className="form-control"/> */}
+        <input type="text" value={msg} onChange={(e)=> dispatch({payload :e.target.value})} className="form-control"/>
+        {/* <input type="text" value={msg} onChange={(e)=> dispatch({name : "Jabed",age : 20})} className="form-control"/> */}
+        <h1>{msg}</h1>
+        </Card.Body>
+      </Card>
+    </Col>
+    </Row>
+  </Container>
   )
 }
 
